ab3c1e42683ce8e7ff93054821f8f970e0f779a4
Malcolm <wrightmbw01@gmail.com>
2019-05-27 10:00:30 -0400

    Created highscores and changed buttons

---
+++ Pacman/Highscores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public class Highscores
+    {
+        public string name;
+        public int score;
+
+        
+    }
+}
