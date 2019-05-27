05e1ea98594d7a2ca4ff2ab4871e2e1626123c52
avercair564 <avercair564@ed.amdsb.ca>
2019-05-27 09:03:04 -0400

    highscore

--- Pacman/HighScreen.Designer.cs
+++ Pacman/HighScreen.Designer.cs
@@ -35,11 +35,16 @@
             this.thirdLabel = new System.Windows.Forms.Label();
             this.fourthLabel = new System.Windows.Forms.Label();
             this.fifthLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.initial1Label = new System.Windows.Forms.Label();
+            this.initial2Label = new System.Windows.Forms.Label();
+            this.initial3Label = new System.Windows.Forms.Label();
+            this.initial4Label = new System.Windows.Forms.Label();
+            this.initial5Label = new System.Windows.Forms.Label();
+            this.score1Label = new System.Windows.Forms.Label();
+            this.score2Label = new System.Windows.Forms.Label();
+            this.score3Label = new System.Windows.Forms.Label();
+            this.score4Label = new System.Windows.Forms.Label();
+            this.score5Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -119,71 +124,136 @@
             this.fifthLabel.Text = "5TH";
             this.fifthLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // label1
+            // initial1Label
             // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(599, 200);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 45);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "???";
+            this.initial1Label.BackColor = System.Drawing.Color.Transparent;
+            this.initial1Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initial1Label.ForeColor = System.Drawing.Color.Red;
+            this.initial1Label.Location = new System.Drawing.Point(599, 200);
+            this.initial1Label.Name = "initial1Label";
+            this.initial1Label.Size = new System.Drawing.Size(122, 45);
+            this.initial1Label.TabIndex = 7;
+            this.initial1Label.Text = "???";
             // 
-            // label2
+            // initial2Label
             // 
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Orchid;
-            this.label2.Location = new System.Drawing.Point(599, 260);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 45);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "???";
+            this.initial2Label.BackColor = System.Drawing.Color.Transparent;
+            this.initial2Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initial2Label.ForeColor = System.Drawing.Color.Orchid;
+            this.initial2Label.Location = new System.Drawing.Point(599, 260);
+            this.initial2Label.Name = "initial2Label";
+            this.initial2Label.Size = new System.Drawing.Size(122, 45);
+            this.initial2Label.TabIndex = 8;
+            this.initial2Label.Text = "???";
             // 
-            // label3
+            // initial3Label
             // 
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Cyan;
-            this.label3.Location = new System.Drawing.Point(599, 320);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 45);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "???";
+            this.initial3Label.BackColor = System.Drawing.Color.Transparent;
+            this.initial3Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initial3Label.ForeColor = System.Drawing.Color.Cyan;
+            this.initial3Label.Location = new System.Drawing.Point(599, 320);
+            this.initial3Label.Name = "initial3Label";
+            this.initial3Label.Size = new System.Drawing.Size(122, 45);
+            this.initial3Label.TabIndex = 9;
+            this.initial3Label.Text = "???";
             // 
-            // label4
+            // initial4Label
             // 
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.DarkSalmon;
-            this.label4.Location = new System.Drawing.Point(599, 380);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(122, 45);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "???";
+            this.initial4Label.BackColor = System.Drawing.Color.Transparent;
+            this.initial4Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initial4Label.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.initial4Label.Location = new System.Drawing.Point(599, 380);
+            this.initial4Label.Name = "initial4Label";
+            this.initial4Label.Size = new System.Drawing.Size(122, 45);
+            this.initial4Label.TabIndex = 10;
+            this.initial4Label.Text = "???";
             // 
-            // label5
+            // initial5Label
             // 
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Yellow;
-            this.label5.Location = new System.Drawing.Point(599, 440);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(122, 45);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "???";
+            this.initial5Label.BackColor = System.Drawing.Color.Transparent;
+            this.initial5Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initial5Label.ForeColor = System.Drawing.Color.Yellow;
+            this.initial5Label.Location = new System.Drawing.Point(599, 440);
+            this.initial5Label.Name = "initial5Label";
+            this.initial5Label.Size = new System.Drawing.Size(122, 45);
+            this.initial5Label.TabIndex = 11;
+            this.initial5Label.Text = "???";
+            // 
+            // score1Label
+            // 
+            this.score1Label.BackColor = System.Drawing.Color.Transparent;
+            this.score1Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score1Label.ForeColor = System.Drawing.Color.Red;
+            this.score1Label.Location = new System.Drawing.Point(323, 200);
+            this.score1Label.Name = "score1Label";
+            this.score1Label.Size = new System.Drawing.Size(122, 45);
+            this.score1Label.TabIndex = 12;
+            this.score1Label.Text = "0000";
+            this.score1Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // score2Label
+            // 
+            this.score2Label.BackColor = System.Drawing.Color.Transparent;
+            this.score2Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score2Label.ForeColor = System.Drawing.Color.Orchid;
+            this.score2Label.Location = new System.Drawing.Point(323, 260);
+            this.score2Label.Name = "score2Label";
+            this.score2Label.Size = new System.Drawing.Size(122, 45);
+            this.score2Label.TabIndex = 13;
+            this.score2Label.Text = "0000";
+            this.score2Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // score3Label
+            // 
+            this.score3Label.BackColor = System.Drawing.Color.Transparent;
+            this.score3Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score3Label.ForeColor = System.Drawing.Color.Cyan;
+            this.score3Label.Location = new System.Drawing.Point(323, 320);
+            this.score3Label.Name = "score3Label";
+            this.score3Label.Size = new System.Drawing.Size(122, 45);
+            this.score3Label.TabIndex = 14;
+            this.score3Label.Text = "0000";
+            this.score3Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // score4Label
+            // 
+            this.score4Label.BackColor = System.Drawing.Color.Transparent;
+            this.score4Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score4Label.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.score4Label.Location = new System.Drawing.Point(323, 380);
+            this.score4Label.Name = "score4Label";
+            this.score4Label.Size = new System.Drawing.Size(122, 45);
+            this.score4Label.TabIndex = 15;
+            this.score4Label.Text = "0000";
+            this.score4Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // score5Label
+            // 
+            this.score5Label.BackColor = System.Drawing.Color.Transparent;
+            this.score5Label.Font = new System.Drawing.Font("mono 07_66", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score5Label.ForeColor = System.Drawing.Color.Yellow;
+            this.score5Label.Location = new System.Drawing.Point(323, 440);
+            this.score5Label.Name = "score5Label";
+            this.score5Label.Size = new System.Drawing.Size(122, 45);
+            this.score5Label.TabIndex = 16;
+            this.score5Label.Text = "0000";
+            this.score5Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // HighScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.score5Label);
+            this.Controls.Add(this.score4Label);
+            this.Controls.Add(this.score3Label);
+            this.Controls.Add(this.score2Label);
+            this.Controls.Add(this.score1Label);
+            this.Controls.Add(this.initial5Label);
+            this.Controls.Add(this.initial4Label);
+            this.Controls.Add(this.initial3Label);
+            this.Controls.Add(this.initial2Label);
+            this.Controls.Add(this.initial1Label);
             this.Controls.Add(this.fifthLabel);
             this.Controls.Add(this.fourthLabel);
             this.Controls.Add(this.thirdLabel);
@@ -206,10 +276,15 @@
         private System.Windows.Forms.Label thirdLabel;
         private System.Windows.Forms.Label fourthLabel;
         private System.Windows.Forms.Label fifthLabel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label initial1Label;
+        private System.Windows.Forms.Label initial2Label;
+        private System.Windows.Forms.Label initial3Label;
+        private System.Windows.Forms.Label initial4Label;
+        private System.Windows.Forms.Label initial5Label;
+        private System.Windows.Forms.Label score1Label;
+        private System.Windows.Forms.Label score2Label;
+        private System.Windows.Forms.Label score3Label;
+        private System.Windows.Forms.Label score4Label;
+        private System.Windows.Forms.Label score5Label;
     }
 }
